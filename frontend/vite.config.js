2ba0355715c699e49528a1a65550d718af4f3a4f
Kamil-Kamil2 <2Kamilhil@gmail.com>
2025-07-29 17:20:10 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   plugins: [react()],
   // The 'base' path needs to be correct for GitHub Pages
   // It should be '/React-Movies/' (your repo name)
-  base: "/React-Movies/", // This is correct if your repo is 'React-Movies'
+  base: "/ReactMovies/", // This is correct if your repo is 'React-Movies'
   // No 'root' or 'outDir' needed here if index.html is in 'frontend/' and you build from 'frontend/'
 });
