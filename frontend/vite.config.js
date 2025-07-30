0e083421343ec4ec18ebd15468e7a3e97f645f64
Kamil Kamil <2Kamilhil@gmail.com>
2025-07-29 17:03:34 -0700

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Movies/"
-})
+  // The 'base' path needs to be correct for GitHub Pages
+  // It should be '/React-Movies/' (your repo name)
+  base: "/React-Movies/", // This is correct if your repo is 'React-Movies'
+  // No 'root' or 'outDir' needed here if index.html is in 'frontend/' and you build from 'frontend/'
+});
